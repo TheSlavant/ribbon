31aed7298ce672d34c85f8eb90f16ba438069a4a
claorisel <claorisel@gmail.com>
2017-03-23 19:40:47 +0000

    registration in progress

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^feed/', views.feed, name ='feed'),
+    url(r'^register/', views.register, name='register'),
 ]
