8f0832b4abf44bac323875e42a21bffd73fe4e2d
claorisel <claorisel@gmail.com>
2017-04-30 23:52:46 +0400

    continue reg

--- accounts/views.py
+++ accounts/views.py
@@ -7,20 +7,49 @@ from django.contrib.auth import (
     login,
     logout,
     )
-from django.shortcuts import render
-from .forms import UserLoginForm
+from django.shortcuts import render, redirect
+from .forms import UserLoginForm, UserRegisterForm
+
+def home_view(request):
+    return render(request, "home.html", {})
 
 def login_view(request):
-    title = "login"
+    print(request.user.is_authenticated())
+    next = request.GET.get('next')
+    title = "Login"
     form = UserLoginForm(request.POST or None)
     if form.is_valid():
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get('password')
-
-    return render(request, "login_form.html", {"form":form, "title":title})
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect("/")
+    return render(request, "login.html", {"form":form, "title": title})
 
 def register_view(request):
-    return render(request, "login.html", {})
+    print(request.user.is_authenticated())
+    next = request.GET.get('next')
+    title = "Register"
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        if next:
+            return redirect(next)
+        return redirect("/")
+
+    context = {
+        "form": form,
+        "title": title
+    }
+    return render(request, "signup.html", context)
 
 def logout_view(request):
+    logout(request)
     return render(request, "login.html", {})
